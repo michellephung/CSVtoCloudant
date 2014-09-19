fc963a7fa6e20b66b9a2323622feffc7bdc98265
Michelle Phung <michellephung@gmail.com>
2014-09-19 13:54:01 -0400

    conversion to backbone.js mvc framework

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,86 +1,90 @@
 //main.js
 $(function(){
-	dragAndDrop = function(){
-		
 
-		// file drag hover
-		function FileDragHover(e) {
-			StopEvents(e);
-			console.log("1"+e.type);
+	var app = {};
 
-			document.getElementById("file-drop-box").className = (e.type == "dragover" ? "hover" : "");
-			$('#drop-mask').show();
-		}
+	//--------------
+    // Models
+    //--------------
+    app.Doc = Backbone.Model.extend({
+    	defaults:{
+    		default: "this is one JSON doc"
+    	}
+    });
 
-		function FileDragLeave(e){
-			
-			StopEvents(e);
-			document.getElementById("file-drop-box").className = "";
+    //--------------
+    // Collections
+    //--------------
+    app.Docs = Backbone.Collection.extend({
+    	model: app.Doc
+    });
+
+    app.docs = new app.Docs();
+
+    //--------------
+    // Views
+    //--------------
+    app.FileDrop= Backbone.View.extend({
+    	el: "#file-drop-box",
+    	initialize: function(){
+    		var dropmask =$("#drop-mask"),
+                filedrag = $("#file-drop-box");
+			var xhr = new XMLHttpRequest();
+			filedrag.css("display", "block");
+    	},
+    	events: {
+    		'dragover': 'FileDragHover',
+    		'dragleave .drop-mask': 'FileDragLeave',
+    		'drop .drop-mask': 'FileDrop'
+    	},
+    	FileDragHover: function(e){
+    		this.StopEvents(e);
+            console.log("hover");
+			e.type == "dragover" ? $("#file-drop-box").addClass("hover") :$("#file-drop-box").removeClass();
+            $('#drop-mask').show();
+    	},
+    	FileDragLeave: function(e){
+    		this.StopEvents(e);
+            console.log("!!leave");
+			$("#file-drop-box").removeClass();
 			$('#drop-mask').hide();
-		}
-
-		// file selection
-		function FileDrop(e) {
-
-			StopEvents(e);
+    	},
+    	FileDrop: function(e){
+    		this.StopEvents(e);
+            console.log("dropped");
 
 			// fetch FileList object
-			var files = document.getElementById("file-drop-box").files || e.dataTransfer.files;
+			var files = e.originalEvent.dataTransfer.files;
 
 			// process all File objects
 			for (var i = 0, f; f = files[i]; i++) {
-				ParseFile(f);
+				this.ParseFile(f);
 			}
 			$("#file-drop-box").removeClass("hover");
 			$('#drop-mask').hide();
-		}
-
-		// output information
-		function Output(msg) {
-			console.log(msg);
-		}
-
-		// output file information
-		function ParseFile(file) {
-			console.log("File information: " + file.name +
+    	},
+    	StopEvents: function(e){
+    		e.stopPropagation();
+			e.preventDefault();
+    	},
+    	ParseFile: function(file){
+    		console.log(
+				"File information: " + file.name +
 				"\ntype: " + file.type +
 				"\nsize: " + file.size +" bytes"
 			);
-		}
-		
-		function StopEvents(e){
-			e.stopPropagation();
-			e.preventDefault();
-		}
+    	}
+    });
 
-		// initialize
-		function Init() {
+    app.AppView = Backbone.View.extend({
+    	el: "#content",
+    	initialize: function(){
+    		var initFilesDrop= new app.FileDrop();
+    	}
+    });
 
-			var dropmask = document.getElementById("drop-mask"),
-				filedrag = document.getElementById("file-drop-box");
-			
-			// is XHR2 available?
-			var xhr = new XMLHttpRequest();
-			if (xhr.upload) {
-
-				// file drop
-				filedrag.addEventListener("dragover", FileDragHover, false);
-				dropmask.addEventListener("dragleave", FileDragLeave, false);
-				dropmask.addEventListener("drop", FileDrop, false);
-
-				filedrag.style.display = "block";
-			}
-		}
-
-		// call initialization file
-		if (window.File && window.FileList && window.FileReader) {
-			Init();
-		}
-	}
-	
-	main = function(){
-		dragAndDrop();
-	}
-
-	main();
+    //--------------
+    // Initializers
+    //-------------- 
+   app.appView = new app.AppView();
 });
